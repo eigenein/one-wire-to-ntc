b6e429e64862fd72d935af3fda891e5d75a229fe
Pavel Perestoronin <eigenein@gmail.com>
2020-02-04 22:22:58 +0100

    New: create project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+constexpr uint8_t ledPin = 13;
+
+void setup() {
+    wdt_disable();
+    wdt_enable(WDTO_8S);
+
+    digitalWrite(ledPin, LOW);
+    pinMode(ledPin, OUTPUT);
+
+    Serial.begin(2000000);
+    while (!Serial);
+}
+
+void loop() {}
